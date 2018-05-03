a088d07ab9bf3c283c7165a35eac414048592ea3
rrrluk <rrrluk@gmail.com>
2018-05-03 23:14:31 +0300

    todo list valmis

--- todoListProject/assets/js/todos.js
+++ todoListProject/assets/js/todos.js
@@ -1,25 +1,30 @@
 // Check off specific todos by clicking
-$("ul").on("click", "li", function() {
+$("ul").on("click", "li", function () {
   $(this).toggleClass("completed");
 });
 
 // Click on X to delete todo
-$("ul").on("click", "span", function(event) {
+$("ul").on("click", "span", function (event) {
   $(this)
     .parent()
-    .fadeOut(500, function() {
+    .fadeOut(500, function () {
       $(this).remove();
     });
   event.stopPropagation();
 });
 
-$("input[type='text']").keypress(function(event) {
+$("input[type='text']").keypress(function (event) {
   if (event.which === 13) {
     // grabbing text from input
     var todoText = $(this).val();
     // clear input
     $(this).val("");
     // create a new li and add to ul
-    $("ul").append("<li><span>X </span>" + todoText + "</li>");
+    $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span>" + todoText + "</li>");
   }
+});
+
+$("#toggle-form").click(function () {
+  $("input[type='text']").slideToggle();
+
 });
